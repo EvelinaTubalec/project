c2a04d515063b7220fb9cb34d934d2f183e26329
evelina.tubalets <evelina.tubalec@gmail.com>
2022-01-15 12:59:42 +0300

    Create service and API for loading users from CSV file.

---
+++ src/main/java/com/leverx/service/LoadingUsersFromCSVFileService.java
@@ -0,0 +1,39 @@
+package com.leverx.service;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+import com.leverx.model.User;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LoadingUsersFromCSVFileService {
+
+  public List<User> findAllFromCSVFile() throws FileNotFoundException {
+    CsvToBean csv = new CsvToBean();
+    String csvFilename = "C:\\Users\\evelina.tubalets\\Desktop\\data.csv";
+    CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+    // Set column mapping strategy
+    List<User> users = new ArrayList<>();
+    List list = csv.parse(setColumMapping(), csvReader);
+    for (Object object : list) {
+      User user = (User) object;
+      users.add(user);
+    }
+    return users;
+  }
+
+  private static ColumnPositionMappingStrategy setColumMapping()
+  {
+    ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
+    strategy.setType(User.class);
+    String[] columns = new String[] {"id", "firstName", "lastName", "email", "password", "jobTitle"};
+    strategy.setColumnMapping(columns);
+    return strategy;
+  }
+}
